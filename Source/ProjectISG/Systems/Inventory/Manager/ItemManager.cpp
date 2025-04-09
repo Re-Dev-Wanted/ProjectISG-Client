259bbba38b52545f499d1be48094049618dffe83
GoldFrosch <goldfrosch@daum.net>
2025-04-10 08:57:49 +0900

    feat: 인벤토리 정보 컴포넌트 추가

---
+++ Source/ProjectISG/Systems/Inventory/Manager/ItemManager.cpp
@@ -0,0 +1,26 @@
+﻿#include "ItemManager.h"
+
+#include "ProjectISG/Systems/Inventory/ItemData.h"
+
+// TODO: 현재 추가 개발 필요한 태스크
+FItemInfoData FItemManager::GetItemInfoById(const UWorld* World,
+                                            const uint16 Id)
+{
+	FItemInfoData NewItemInfoData;
+	return NewItemInfoData;
+}
+
+FItemMetaInfo FItemManager::GetInitialItemMetaDataById
+(const UWorld* World, const uint16 Id)
+{
+	const FItemInfoData InitialData = GetItemInfoById(World, Id);
+	// TODO: 만약 OptionData 기반으로 세팅이 필요하다면 InitialData 기반으로
+	// 여기서 OptionData와 MetaData를 맞춰줄 것
+
+	FItemMetaInfo NewMetaInfo;
+	NewMetaInfo.SetId(Id);
+	NewMetaInfo.SetCurrentCount(1);
+	NewMetaInfo.SetMetaData(InitialData.GetMetaData());
+
+	return NewMetaInfo;
+}
---
+++ Source/ProjectISG/Systems/Inventory/Manager/ItemManager.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+struct FItemMetaInfo;
+struct FItemInfoData;
+
+class FItemManager
+{
+public:
+	static FItemInfoData GetItemInfoById(const UWorld* World, const uint16 Id);
+	static FItemMetaInfo GetInitialItemMetaDataById(
+		const UWorld* World, const uint16 Id);
+};
