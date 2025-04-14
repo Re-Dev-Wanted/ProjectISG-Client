daddcce0f73d4ad903ca067c77b829a560c588ff
Jangjinhyeok <zero9936@gmail.com>
2025-04-14 16:35:26 +0900

    TimeManager 추가, BP_TimeManager 추가

---
+++ Source/ProjectISG/Systems/Time/TimeManager.cpp
@@ -0,0 +1,117 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TimeManager.h"
+
+#include "Components/DirectionalLightComponent.h"
+
+
+ATimeManager::ATimeManager()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Sun = CreateDefaultSubobject<UDirectionalLightComponent>(TEXT("Sun"));
+	if (Sun)
+	{
+		SetRootComponent(Sun);
+		Sun->SetWorldRotation(FRotator(-90,0,0));
+	}
+}
+
+void ATimeManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	for (int32 i = 0; i < 12; i++)
+	{
+		if (i == 0 || i == 2 || i == 4 || i == 6 || i == 7 || i == 9 || i == 11)
+		{
+			DaysInMonths.Add(31);
+		}
+		else if (i == 3 || i == 5 || i == 8 || i == 10)
+		{
+			DaysInMonths.Add(30);
+		}
+		else if (i == 1)
+		{
+			DaysInMonths.Add(28);
+		}
+	}
+}
+
+void ATimeManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!TimeStop)
+	{
+		UpdateCycleTime(DeltaTime);
+		RotateSun();
+	}
+
+	if (GetWorld()->GetFirstPlayerController()->WasInputKeyJustPressed(EKeys::Tab))
+	{
+		TimeStop = !TimeStop;
+	}
+}
+
+void ATimeManager::UpdateCycleTime(float DeltaTime)
+{
+	Seconds += SpeedOfTime * DeltaTime;
+
+	if (Seconds >= 60)
+	{
+		Seconds = 0;
+		Minutes++;
+	}
+
+	if (Minutes >= 60)
+	{
+		Minutes = 0;
+		Hours++;
+	}
+
+	if (Hours >= 24)
+	{
+		Hours = 0;
+		UpdateCycleDate();
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Days : %d, %d:%d:%f"), Days, Hours, Minutes, Seconds);
+}
+
+void ATimeManager::UpdateCycleDate()
+{
+	Days++;
+
+	if (Days > DaysInMonths[Months - 1])
+	{
+		Days = 1;
+		Months++;
+	}
+
+	if (Months > 12)
+	{
+		Months = 1;
+		Year++;
+	}
+}
+
+void ATimeManager::RotateSun()
+{
+	float TotalMinutes = Hours * 60.0f + Minutes;
+
+	float SunPitch = 0.f;
+
+	// 06(해 뜨는 시간) ~ 24(해 지는 시간)을 기준으로 offset을 통해 일출과 일몰 시간을 정해줌
+	SunPitch = FMath::GetMappedRangeValueClamped(
+		FVector2d(0.f, 1440.0f),
+		FVector2d((90.f + (6 - SunriseTime) * 15.f), 360.f + ((24 - SunsetTime) * 15.f)), 
+		TotalMinutes);
+
+	// 0 ~ 360 범위로 정규화
+	SunPitch = FMath::Fmod(SunPitch, 360.0f);
+	FRotator newRot = FRotator(SunPitch, 0.f, 0.f);
+	Sun->SetWorldRotation(newRot);
+}
+
---
+++ Source/ProjectISG/Systems/Time/TimeManager.h
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TimeManager.generated.h"
+
+UCLASS()
+class PROJECTISG_API ATimeManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATimeManager();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+	
+private:
+	void UpdateCycleTime(float DeltaTime);
+
+	void UpdateCycleDate();
+
+	void RotateSun();
+
+public:
+
+private:
+#pragma  region Sun
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Sun")
+	class UDirectionalLightComponent* Sun = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Sun")
+	int32 SunriseTime = 5;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Sun")
+	int32 SunsetTime = 20;
+#pragma endregion 
+	
+#pragma region Time
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Time")
+	float Seconds = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Time")
+	int32 Minutes = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Time")
+	int32 Hours = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Time")
+	int32 Days = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Time")
+	int32 Months = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Time")
+	int32 Year = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Time")
+	TArray<int32> DaysInMonths;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Time")
+	int32 SpeedOfTime = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Time")
+	bool TimeStop = false;
+#pragma endregion
+};
