5ce780c391e9bd82b0280e360fad32aca8b3cf92
GoldFrosch <goldfrosch@daum.net>
2025-04-15 19:26:19 +0900

    feat: 메인 캐릭터 기본 설정 (이동 및 카메라)

---
+++ Source/ProjectISG/Core/Character/BaseCharacter.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacter.generated.h"
+
+UCLASS()
+class PROJECTISG_API ABaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABaseCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(
+		class UInputComponent* PlayerInputComponent) override;
+};
