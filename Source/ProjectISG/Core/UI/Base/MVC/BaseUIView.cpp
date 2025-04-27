a3cf9d4139c30c42ca94920f3b97bbd5834a5657
GoldFrosch <goldfrosch@daum.net>
2025-04-27 15:19:04 +0900

    fix: 기본 Class 없을 시 터지는 이슈 수정

--- Source/ProjectISG/Core/UI/Base/MVC/BaseUIView.cpp
+++ Source/ProjectISG/Core/UI/Base/MVC/BaseUIView.cpp
@@ -9,16 +9,22 @@ void UBaseUIView::NativeConstruct()
 
 	// 일단 View가 생성되는 시점에서 Model과 Controller를 반영한다.
 	// MVC 패턴 특성상 Model을 직접 가져오는 행동은 최대한 자제한다.
-	if (!Model)
+	if (ModelClass)
 	{
-		Model = NewObject<UBaseUIModel>(this, ModelClass);
+		if (!Model)
+		{
+			Model = NewObject<UBaseUIModel>(this, ModelClass);
+		}
 	}
 
-	if (!Controller)
+	if (ControllerClass)
 	{
-		Controller = NewObject<UBaseUIController>(this, ControllerClass);
-	}
+		if (!Controller)
+		{
+			Controller = NewObject<UBaseUIController>(this, ControllerClass);
+		}
 
-	Controller->InitializeController(this, Model);
-	Controller->InitializeSettingToPlayerController(GetOwningPlayer());
+		Controller->InitializeController(this, Model);
+		Controller->InitializeSettingToPlayerController(GetOwningPlayer());
+	}
 }
