45e21c88101fecd0d38d8ed9613b35ea2c49b096
GoldFrosch <goldfrosch@daum.net>
2025-05-03 23:20:00 +0900

    refactor: 버튼 인스턴스화

---
+++ Source/ProjectISG/Core/UI/Base/Module/UI_BaseButton.cpp
@@ -0,0 +1,12 @@
+﻿#include "UI_BaseButton.h"
+
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "ProjectISG/Core/ISGGameInstance.h"
+
+void UUI_BaseButton::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	Text->SetText(TextValue);
+}
