01cefc22d890fff3fca911e096cc99603588ac70
Loenna59 <ginzzang1010@naver.com>
2025-05-06 18:13:33 +0900

    fish data table 생성

---
+++ Source/ProjectISG/Contents/Fishing/FishData.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectISG/Utils/MacroUtil.h"
+#include "UObject/Object.h"
+#include "FishData.generated.h"
+
+UENUM(BlueprintType)
+enum class EFishGrade : uint8
+{
+	Common,
+	Uncommon,
+	Rare,
+	Epic,
+	Legendary,
+	Mythic
+};
+
+USTRUCT(BlueprintType)
+struct PROJECTISG_API FFishData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	GETTER(uint16, Id);
+	GETTER(uint16, ItemId);
+	GETTER(TSoftObjectPtr<USkeletalMesh>, Mesh);
+	GETTER(EFishGrade, Grade);
+	GETTER(float, WaitTimeMin)
+	GETTER(float, WaitTimeMax)
+
+protected:
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	uint16 Id = 0;
+	
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	uint16 ItemId = 0;
+
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	TSoftObjectPtr<USkeletalMesh> Mesh;
+
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	EFishGrade Grade = EFishGrade::Common;
+
+	UPROPERTY(EditDefaultsOnly, meta = (ClampMin = 2.f, ClampMax = 5.f, AllowPrivateAccess = true))
+	float WaitTimeMin = 2.f;
+
+	UPROPERTY(EditDefaultsOnly, meta = (ClampMin = 5.f, ClampMax = 20.f, AllowPrivateAccess = true))
+	float WaitTimeMax = 5.f;
+};
---
+++ Source/ProjectISG/Contents/Fishing/Managers/FishingManager.cpp
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FishingManager.h"
+
+#include "ProjectISG/Utils/ArrayUtil.h"
+
+bool UFishingManager::IsInitialized;
+TArray<FFishData> UFishingManager::Datas;
+TMap<uint16, FFishData> UFishingManager::DataMap;
+
+void UFishingManager::Initialize()
+{
+	if (IsInitialized)
+	{
+		return;
+	}
+
+	IsInitialized = true;
+
+	const static ConstructorHelpers::FObjectFinder<UDataTable>
+		ItemInfoDataTable(TEXT("/Game/Contents/Fishing/Data/DT_FishData.DT_FishData"));
+
+	if (ItemInfoDataTable.Succeeded())
+	{
+		TArray<FFishData*> TempItemInfoList;
+		ItemInfoDataTable.Object->GetAllRows<FFishData>(TEXT(""), TempItemInfoList);
+
+		for (const FFishData* InfoItem : TempItemInfoList)
+		{
+			Datas.Add(*InfoItem);
+			DataMap.Add(InfoItem->GetId(), *InfoItem);
+		}
+	}
+}
+
+FFishData UFishingManager::GetRandomData()
+{
+	if (!IsInitialized)
+	{
+		Initialize();
+	}
+
+	TArray Arr(Datas);
+	ArrayUtil::KnuthShuffle(Arr);
+
+	return Arr[0];
+}
+
+FFishData UFishingManager::GetDataById(uint16 Id)
+{
+	if (DataMap.Contains(Id))
+	{
+		return DataMap[Id];
+	}
+
+	return FFishData();
+}
+
+FFishData UFishingManager::GetDataByItemId(uint16 ItemId)
+{
+	for (FFishData Data : Datas)
+	{
+		if (Data.GetItemId() == ItemId)
+		{
+			return Data;
+		}
+	}
+
+	return FFishData();
+}
+
+
