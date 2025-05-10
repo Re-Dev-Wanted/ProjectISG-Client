eca57344704fc17050c484d6ac32f46e46959577
GoldFrosch <goldfrosch@daum.net>
2025-05-10 16:28:41 +0900

    refactor: 불필요한 코드 제거

--- Source/ProjectISG/Core/Character/BaseCharacter.cpp
+++ Source/ProjectISG/Core/Character/BaseCharacter.cpp
@@ -29,16 +29,3 @@ UAbilitySystemComponent* ABaseCharacter::GetAbilitySystemComponent() const
 {
 	return AbilitySystemComponent;
 }
-
-void ABaseCharacter::PlayCinematic(ULevelSequencePlayer* Cinematic)
-{
-	UE_LOG(LogTemp, Display, TEXT("클라, 서버 여부 %d"), HasAuthority());
-	Cinematic->Play();
-}
-
-void ABaseCharacter::Client_PlayCinematic_Implementation(
-	ULevelSequencePlayer* Cinematic)
-{
-	UE_LOG(LogTemp, Display, TEXT("너 호출 안되는거 같아... %p"), Cinematic);
-	PlayCinematic(Cinematic);
-}
