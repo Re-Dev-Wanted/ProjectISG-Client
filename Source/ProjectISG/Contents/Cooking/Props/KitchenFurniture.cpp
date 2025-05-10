7f24edb7d7e7b8844ad034f8d27e5d33e2417cd5
GoldFrosch <goldfrosch@daum.net>
2025-05-10 21:11:56 +0900

    refactor: 시네마틱 RPC 통신으로 처리

--- Source/ProjectISG/Contents/Cooking/Props/KitchenFurniture.cpp
+++ Source/ProjectISG/Contents/Cooking/Props/KitchenFurniture.cpp
@@ -1,16 +1,10 @@
 ﻿#include "KitchenFurniture.h"
 
 #include "Camera/CameraComponent.h"
-#include "LevelSequencePlayer.h"
-#include "LevelSequenceActor.h"
-#include "MovieSceneSequencePlaybackSettings.h"
-#include "GameFramework/CharacterMovementComponent.h"
+#include "Component/KitchenFurnitureCinematicComponent.h"
 #include "ProjectISG/Contents/Cooking/CookingStruct.h"
 #include "ProjectISG/Core/Character/Player/MainPlayerCharacter.h"
-#include "ProjectISG/Core/Controller/MainPlayerController.h"
 #include "ProjectISG/Core/Character/Player/Component/InteractionComponent.h"
-#include "ProjectISG/Systems/Logging/LoggingStruct.h"
-#include "ProjectISG/Systems/Logging/LoggingSubSystem.h"
 
 AKitchenFurniture::AKitchenFurniture()
 {
@@ -23,24 +17,31 @@ AKitchenFurniture::AKitchenFurniture()
 
 	KitchenCameraComponent = CreateDefaultSubobject<UCameraComponent>(
 		"Kitchen Camera Component");
+	KitchenCameraComponent->SetRelativeLocation({140.0, 90.0, 90.0});
+	KitchenCameraComponent->SetRelativeRotation({0.0, 180.0, 0.0});
 	KitchenCameraComponent->SetupAttachment(Mesh);
 
-	FryPanMesh = CreateDefaultSubobject<UStaticMeshComponent>(
-		"Fry Pan Mesh");
+	FryPanMesh = CreateDefaultSubobject<UStaticMeshComponent>("Fry Pan Mesh");
 	FryPanMesh->SetupAttachment(GetRootComponent());
 
-	ScoopMesh = CreateDefaultSubobject<UStaticMeshComponent>(
-		"Scoop Mesh");
+	ScoopMesh = CreateDefaultSubobject<UStaticMeshComponent>("Scoop Mesh");
 	ScoopMesh->SetupAttachment(GetRootComponent());
 
-	WokMesh = CreateDefaultSubobject<UStaticMeshComponent>(
-		"Wok Mesh");
+	WokMesh = CreateDefaultSubobject<UStaticMeshComponent>("Wok Mesh");
 	WokMesh->SetupAttachment(GetRootComponent());
+
+	KitchenFurnitureCinematicComponent = CreateDefaultSubobject<
+		UKitchenFurnitureCinematicComponent>(
+		"Kitchen Furniture Cinematic Component");
+
+	bReplicates = true;
+	bAlwaysRelevant = true;
+	SetReplicatingMovement(true);
 }
 
 bool AKitchenFurniture::GetCanInteractive() const
 {
-	return UsingCharacter == nullptr;
+	return GetOwner() == nullptr;
 }
 
 FString AKitchenFurniture::GetInteractiveDisplayText() const
@@ -54,7 +55,10 @@ void AKitchenFurniture::OnInteractive(AActor* Causer)
 
 	if (AMainPlayerCharacter* Player = Cast<AMainPlayerCharacter>(Causer))
 	{
-		UsingCharacter = Player;
+		// RPC 함수를 위한 Owner 세팅
+		SetOwner(Player);
+
+		SetUsingOwner(Player);
 
 		Player->bUseControllerRotationYaw = false;
 		Player->SetActorTransform(
@@ -69,70 +73,54 @@ void AKitchenFurniture::OnInteractive(AActor* Causer)
 		Player->GetInteractionComponent()->SetIsInteractive(false);
 		Player->GetInteractionComponent()->SetSelectedInteractiveActor(this);
 
-		// TODO: RPC 함수 호출로 모든 곳에서 동일한
-		// 동작을 실행시켜야 한다.
-		StartCookingMode();
+		PlayCookingCinematic(
+			Player, EKitchenFurnitureCinematicStatus::StartCooking);
 	}
 }
 
-void AKitchenFurniture::StartCookingMode()
+void AKitchenFurniture::PlayCookingCinematic(AMainPlayerCharacter* Target
+											, const
+											EKitchenFurnitureCinematicStatus
+											Status)
 {
-	AMainPlayerController* PC = UsingCharacter->GetController<
-		AMainPlayerController>();
-
-	FMovieSceneSequencePlaybackSettings PlaybackSettings;
-	PlaybackSettings.bAutoPlay = true;
-
-	LevelSequencePlayer = ULevelSequencePlayer::CreateLevelSequencePlayer(
-		GetWorld(), StartCookingCinematic,
-		PlaybackSettings, LevelSequenceActor);
-
-	LevelSequencePlayer->OnFinished.AddDynamic(
-		this, &ThisClass::OnFinishStartCooking);
-
-	UsingCharacter->GetCharacterMovement()->DisableMovement();
-
-	PC->SetIgnoreLookInput(true);
-	PC->SetViewTargetWithBlend(this, 0.5f);
-
-	GetKitchenCameraComponent()->Activate();
-
-	LevelSequenceActor->AddBindingByTag(FName(TEXT("Player")), UsingCharacter);
-	LevelSequenceActor->AddBindingByTag(FName(TEXT("Interactive")), this);
-
-	LevelSequenceActor->SetActorTransform(UsingCharacter->GetTransform());
-
-	LevelSequencePlayer->Play();
+	if (HasAuthority())
+	{
+		Multicast_PlayCookingCinematic(Target, Status);
+	}
+	else
+	{
+		// 알수 없는 이유로 클라이언트에서 RPC 통신이 안됨
+		Server_PlayCookingCinematic(Target, Status);
+	}
 }
 
-void AKitchenFurniture::EndCookingMode()
+void AKitchenFurniture::PlayCookingCinematic_Internal(
+	AMainPlayerCharacter* Target, const EKitchenFurnitureCinematicStatus Status)
 {
-	AMainPlayerController* PC = UsingCharacter->GetController<
-		AMainPlayerController>();
+	switch (Status)
+	{
+	case EKitchenFurnitureCinematicStatus::StartCooking:
+		{
+			KitchenFurnitureCinematicComponent->StartCookingMode(Target);
+			break;
+		}
+	default:
+		{
+			break;
+		}
+	}
+}
 
-	FMovieSceneSequencePlaybackSettings PlaybackSettings;
-	PlaybackSettings.bAutoPlay = true;
+void AKitchenFurniture::Server_PlayCookingCinematic_Implementation(
+	AMainPlayerCharacter* Target, const EKitchenFurnitureCinematicStatus Status)
+{
+	Multicast_PlayCookingCinematic(Target, Status);
+}
 
-	LevelSequencePlayer = ULevelSequencePlayer::CreateLevelSequencePlayer(
-		GetWorld(), EndCookingCinematic,
-		PlaybackSettings, LevelSequenceActor);
-
-	LevelSequencePlayer->OnFinished.AddDynamic(
-		this, &ThisClass::OnFinishEndCooking);
-
-	UsingCharacter->GetCharacterMovement()->DisableMovement();
-
-	PC->SetIgnoreLookInput(true);
-	PC->SetViewTargetWithBlend(this, 0.5f);
-
-	GetKitchenCameraComponent()->Activate();
-
-	LevelSequenceActor->AddBindingByTag(FName(TEXT("Player")), UsingCharacter);
-	LevelSequenceActor->AddBindingByTag(FName(TEXT("Interactive")), this);
-
-	LevelSequenceActor->SetActorTransform(UsingCharacter->GetTransform());
-
-	LevelSequencePlayer->Play();
+void AKitchenFurniture::Multicast_PlayCookingCinematic_Implementation(
+	AMainPlayerCharacter* Target, const EKitchenFurnitureCinematicStatus Status)
+{
+	PlayCookingCinematic_Internal(Target, Status);
 }
 
 void AKitchenFurniture::EquipCookingToolToAct(
@@ -143,23 +131,23 @@ void AKitchenFurniture::EquipCookingToolToAct(
 	case ECookingTool::FryingPan:
 		{
 			FryPanMesh->SetVisibility(true);
-			FryPanMesh->AttachToComponent(Params.AttachPoint,
-			                              FAttachmentTransformRules::SnapToTargetIncludingScale,
-			                              TEXT("hand_r"));
+			FryPanMesh->AttachToComponent(Params.AttachPoint
+										, FAttachmentTransformRules::SnapToTargetIncludingScale
+										, TEXT("hand_r"));
 			break;
 		}
 	case ECookingTool::Wok:
 		{
 			ScoopMesh->SetVisibility(true);
-			ScoopMesh->AttachToComponent(Params.AttachPoint,
-			                             FAttachmentTransformRules::SnapToTargetIncludingScale,
-			                             TEXT("scoop_socket"));
+			ScoopMesh->AttachToComponent(Params.AttachPoint
+										, FAttachmentTransformRules::SnapToTargetIncludingScale
+										, TEXT("scoop_socket"));
 			ScoopMesh->SetRelativeRotation(FRotator::ZeroRotator);
 
 			WokMesh->SetVisibility(true);
-			WokMesh->AttachToComponent(Params.AttachPoint,
-			                           FAttachmentTransformRules::SnapToTargetIncludingScale,
-			                           TEXT("wok_socket"));
+			WokMesh->AttachToComponent(Params.AttachPoint
+										, FAttachmentTransformRules::SnapToTargetIncludingScale
+										, TEXT("wok_socket"));
 			WokMesh->SetRelativeRotation(FRotator::ZeroRotator);
 
 			break;
@@ -174,65 +162,17 @@ void AKitchenFurniture::EquipCookingToolToAct(
 void AKitchenFurniture::UnEquipCookingToolToAct()
 {
 	FryPanMesh->SetVisibility(false);
-	FryPanMesh->AttachToComponent(GetRootComponent(),
-	                              FAttachmentTransformRules::SnapToTargetIncludingScale);
+	FryPanMesh->AttachToComponent(GetRootComponent()
+								, FAttachmentTransformRules::SnapToTargetIncludingScale);
 	FryPanMesh->SetRelativeLocation(FVector::ZeroVector);
 
 	ScoopMesh->SetVisibility(false);
-	ScoopMesh->AttachToComponent(GetRootComponent(),
-	                             FAttachmentTransformRules::SnapToTargetIncludingScale);
+	ScoopMesh->AttachToComponent(GetRootComponent()
+								, FAttachmentTransformRules::SnapToTargetIncludingScale);
 	ScoopMesh->SetRelativeLocation(FVector::ZeroVector);
 
 	WokMesh->SetVisibility(false);
-	WokMesh->AttachToComponent(GetRootComponent(),
-	                           FAttachmentTransformRules::SnapToTargetIncludingScale);
+	WokMesh->AttachToComponent(GetRootComponent()
+								, FAttachmentTransformRules::SnapToTargetIncludingScale);
 	WokMesh->SetRelativeLocation(FVector::ZeroVector);
-}
-
-void AKitchenFurniture::OnFinishStartCooking()
-{
-	AMainPlayerController* PC = UsingCharacter->GetController<
-		AMainPlayerController>();
-	PC->PushUI(EUIName::Popup_CookingRecipeUI);
-}
-
-void AKitchenFurniture::OnFinishEndCooking()
-{
-	AMainPlayerController* PC = UsingCharacter->GetController<
-		AMainPlayerController>();
-	PC->PushUI(EUIName::Gameplay_MainHUD);
-	PC->SetIgnoreLookInput(false);
-	PC->SetViewTargetWithBlend(UsingCharacter, 0.5f);
-
-	UsingCharacter->bUseControllerRotationYaw = true;
-	UsingCharacter->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
-	UsingCharacter->GetCameraComponent()->Activate();
-
-	UsingCharacter->GetInteractionComponent()->SetIsInteractive(true);
-	UsingCharacter->GetInteractionComponent()->
-	                SetSelectedInteractiveActor(nullptr);
-
-	UsingCharacter = nullptr;
-}
-
-void AKitchenFurniture::LoggingStartCooking()
-{
-	FDiaryLogParams LogParams;
-	LogParams.Location = "요리장";
-	LogParams.ActionType = ELoggingActionType::COOKING;
-	LogParams.ActionName = ELoggingActionName::start_cooking;
-
-	GetWorld()->GetGameInstance()->GetSubsystem<ULoggingSubSystem>()->
-	            LoggingData(LogParams);
-}
-
-void AKitchenFurniture::LoggingEndCooking()
-{
-	FDiaryLogParams LogParams;
-	LogParams.Location = "요리장";
-	LogParams.ActionType = ELoggingActionType::COOKING;
-	LogParams.ActionName = ELoggingActionName::finish_cooking;
-
-	GetWorld()->GetGameInstance()->GetSubsystem<ULoggingSubSystem>()->
-	            LoggingData(LogParams);
 }
--- Source/ProjectISG/GAS/Common/Object/BaseInteractiveActor.cpp
+++ Source/ProjectISG/GAS/Common/Object/BaseInteractiveActor.cpp
@@ -1,5 +1,29 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BaseInteractiveActor.h"
 
+#include "Net/UnrealNetwork.h"
+
+void ABaseInteractiveActor::GetLifetimeReplicatedProps(
+	TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ABaseInteractiveActor, UsingCharacter)
+}
+
+void ABaseInteractiveActor::SetUsingOwner(AMainPlayerCharacter* NewOwner)
+{
+	if (HasAuthority())
+	{
+		SetUsingCharacter(NewOwner);
+	}
+	else
+	{
+		Server_SetUsingOwner(NewOwner);
+	}
+}
+
+void ABaseInteractiveActor::Server_SetUsingOwner_Implementation(
+	AMainPlayerCharacter* NewOwner)
+{
+	SetUsingCharacter(NewOwner);
+}
