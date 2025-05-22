25105d0ffaaa202b12f4b45dba81ecb156beb907
GoldFrosch <goldfrosch@daum.net>
2025-05-22 20:38:36 +0900

    chore: 디자인 변경 및 대응 로직 추가

--- Source/ProjectISG/Core/UI/Popup/Diary/UI/DiaryList/UIV_DiaryListUI.h
+++ Source/ProjectISG/Core/UI/Popup/Diary/UI/DiaryList/UIV_DiaryListUI.h
@@ -16,14 +16,19 @@ class PROJECTISG_API UUIV_DiaryListUI : public UBaseUIView
 
 public:
 	GETTER(TObjectPtr<UTextBlock>, DiaryDayText)
+	GETTER(TObjectPtr<UTextBlock>, UserId)
 	GETTER(TObjectPtr<UMultiLineEditableTextBox>, DiaryDescription)
 	GETTER(TObjectPtr<UButton>, PrevButton)
 	GETTER(TObjectPtr<UButton>, NextButton)
+	GETTER(TObjectPtr<UButton>, ExitButton)
 	GETTER(TObjectPtr<UUI_HttpImage>, DiaryImage)
 
 private:
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UTextBlock> DiaryDayText;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> UserId;
 
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UMultiLineEditableTextBox> DiaryDescription;
@@ -33,6 +38,9 @@ private:
 
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UButton> NextButton;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> ExitButton;
 
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UUI_HttpImage> DiaryImage;
