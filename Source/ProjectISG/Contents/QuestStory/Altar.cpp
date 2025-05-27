ba60582a60504b2b9ccef9b2326c3697432a76ff
Jangjinhyeok <zero9936@gmail.com>
2025-05-27 10:39:11 +0900

    제단 레시피, 제단 추가

---
+++ Source/ProjectISG/Contents/QuestStory/Altar.cpp
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Altar.h"
+
+#include "ProjectISG/Core/Character/Player/MainPlayerCharacter.h"
+#include "ProjectISG/Core/Controller/MainPlayerController.h"
+#include "ProjectISG/Core/PlayerState/MainPlayerState.h"
+#include "ProjectISG/Systems/Inventory/Components/InventoryComponent.h"
+
+
+AAltar::AAltar()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+}
+
+void AAltar::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAltar::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AAltar::OnInteractive(AActor* Causer)
+{
+	Super::OnInteractive(Causer);
+
+	AMainPlayerCharacter* Player = Cast<AMainPlayerCharacter>(Causer);
+	AMainPlayerState* PS = Player->GetPlayerState<AMainPlayerState>();
+	AMainPlayerController* PC = Player->GetController<AMainPlayerController>();
+	
+	if (Player && PS)
+	{
+		if (PS->GetInventoryComponent()->HasItemInInventory(OfferingFoodId, 1))
+		{
+			// 공물 음식 삭제
+			PS->GetInventoryComponent()->RemoveItem(OfferingFoodId, 1);
+			// 퀘스트 완료
+			PC->SetCustomQuestComplete(true);
+			// 엔딩처리
+			UE_LOG(LogTemp, Warning, TEXT("엔딩 처리"));
+		}
+	}
+}
+
+FString AAltar::GetInteractiveDisplayText() const
+{
+	return TEXT("제단에 공물 바치기");
+}
+
+bool AAltar::GetCanInteractive() const
+{
+	return true;
+}
+
---
+++ Source/ProjectISG/Contents/QuestStory/Altar.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectISG/GAS/Common/Object/BaseInteractiveActor.h"
+#include "ProjectISG/Systems/Grid/Actors/Placement.h"
+#include "Altar.generated.h"
+
+UCLASS()
+class PROJECTISG_API AAltar : public APlacement
+{
+	GENERATED_BODY()
+
+public:
+	AAltar();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void OnInteractive(AActor* Causer) override;
+
+	virtual FString GetInteractiveDisplayText() const override;
+
+	virtual bool GetCanInteractive() const override;
+	
+private:
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = true),
+		Category = Setting)
+	uint32 OfferingFoodId;
+
+};
