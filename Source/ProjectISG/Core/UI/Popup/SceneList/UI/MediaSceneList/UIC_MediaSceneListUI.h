ca5ddd5337812a9a3f276208a8673c09a665eceb
GoldFrosch <goldfrosch@daum.net>
2025-05-28 19:09:28 +0900

    refactor: 파일 이름 변경

---
+++ Source/ProjectISG/Core/UI/Popup/SceneList/UI/MediaSceneList/UIC_MediaSceneListUI.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectISG/Core/UI/Base/MVC/BaseUIController.h"
+#include "UIC_MediaSceneListUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTISG_API UUIC_MediaSceneListUI : public UBaseUIController
+{
+	GENERATED_BODY()
+};
---
+++ Source/ProjectISG/Core/UI/Popup/SceneList/UI/MediaSceneList/UIV_MediaSceneListUI.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectISG/Core/UI/Base/MVC/BaseUIView.h"
+#include "UIV_MediaSceneList.generated.h"
+
+UCLASS()
+class PROJECTISG_API UUIV_MediaSceneList : public UBaseUIView
+{
+	GENERATED_BODY()
+};
