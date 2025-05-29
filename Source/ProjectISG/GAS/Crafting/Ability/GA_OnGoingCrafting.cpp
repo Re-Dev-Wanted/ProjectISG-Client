2dd8bdec2c792a1733e1535e4cfac9968d2ad0ed
loenna <arterycutter@gmail.com>
2025-05-29 21:02:09 +0900

    몽타지 추가

--- Source/ProjectISG/GAS/Crafting/Ability/GA_OnGoingCrafting.cpp
+++ Source/ProjectISG/GAS/Crafting/Ability/GA_OnGoingCrafting.cpp
@@ -2,22 +2,29 @@
 
 #include "ProjectISG/Contents/Building/Props/Workbench.h"
 #include "ProjectISG/Core/Character/Player/MainPlayerCharacter.h"
+#include "ProjectISG/Core/PlayerState/MainPlayerState.h"
 #include "ProjectISG/Core/Controller/MainPlayerController.h"
 #include "ProjectISG/Core/UI/Base/Components/UIManageComponent.h"
 #include "ProjectISG/Core/UI/Popup/Crafting/UI/UIC_WorkbenchUI.h"
 #include "ProjectISG/GAS/Common/Ability/Utility/PlayMontageWithEvent.h"
+#include "ProjectISG/Systems/Animation/Manager/AnimMontageManager.h"
 
 void UGA_OnGoingCrafting::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
                                           const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	const AMainPlayerCharacter* Player = Cast<AMainPlayerCharacter>(
+		ActorInfo->AvatarActor.Get());
 	
 	AT_StartMontageEvent = UPlayMontageWithEvent::InitialEvent
 	(
 		this,
 		NAME_None,
-		CraftingAnimMontage,
+		UAnimMontageManager::GetMontage(
+			Player->GetPlayerState<AMainPlayerState>(),
+			EAnimMontageKey::Crafting),
 		FGameplayTagContainer()
 	);
 
@@ -44,7 +51,11 @@ void UGA_OnGoingCrafting::EndMontage(FGameplayTag EventTag, FGameplayEventData P
 		Player->GetController());
 
 	AMainPlayerCharacter* _Player = const_cast<AMainPlayerCharacter*>(Player);
-	_Player->PlayAnimMontage(IdleAnimMontage);
+	_Player->PlayAnimMontage(
+		UAnimMontageManager::GetMontage(
+			Player->GetPlayerState<AMainPlayerState>(),
+			EAnimMontageKey::Idle_OnTable)
+	);
 
 	if (Player->IsLocallyControlled())
 	{
