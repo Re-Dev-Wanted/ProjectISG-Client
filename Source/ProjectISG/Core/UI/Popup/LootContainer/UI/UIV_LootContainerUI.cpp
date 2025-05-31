c60e164ac1349fc46db6a5383c98edf3eba620d9
GoldFrosch <goldfrosch@daum.net>
2025-05-31 19:16:00 +0900

    fix: 상자 UI 정리

--- Source/ProjectISG/Core/UI/Popup/LootContainer/UI/UIV_LootContainerUI.cpp
+++ Source/ProjectISG/Core/UI/Popup/LootContainer/UI/UIV_LootContainerUI.cpp
@@ -1,12 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "UIV_LootContainerUI.h"
 
-#include "Components/TextBlock.h"
 #include "Views/LootContainerListView.h"
 
-void UUIV_LootContainerUI::SetContainer(const TArray<FItemMetaInfo>& Items, TScriptInterface<IItemHandler> Handler) const
+void UUIV_LootContainerUI::SetContainer(const TArray<FItemMetaInfo>& Items
+										, TScriptInterface<IItemHandler>
+										Handler) const
 {
 	ContainerView->SetContainerInfo(InventorySlotClass, Items, Handler);
 }
