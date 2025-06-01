0bd7dee31d002f5f6720b1b1dfff6fbbf701e25d
Loenna59 <ginzzang1010@naver.com>
2025-06-02 03:41:31 +0900

    [크래프팅] 뚝딱 거릴 때 파티클

---
+++ Source/ProjectISG/GAS/Common/Notify/AnimNotify_AttachMesh.cpp
@@ -0,0 +1,30 @@
+﻿#include "AnimNotify_AttachMesh.h"
+
+void UAnimNotify_AttachMesh::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                         float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	if (MeshComp)
+	{
+		// if (!AttachedMeshComponent)
+		// {
+		// 	AttachedMeshComponent = NewObject<UStaticMeshComponent>(this);
+		// 	AttachedMeshComponent->RegisterComponent();
+		// }
+		// AttachedMeshComponent->SetStaticMesh(StaticMesh);
+		// AttachedMeshComponent->AttachToComponent(MeshComp, FAttachmentTransformRules::SnapToTargetIncludingScale, SocketName);
+	}
+}
+
+void UAnimNotify_AttachMesh::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	// if (AttachedMeshComponent)
+	// {
+	// 	AttachedMeshComponent->DestroyComponent();
+	// 	AttachedMeshComponent = nullptr;
+	// }
+}
