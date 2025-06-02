9b94a57ab4b7da5b7647b03018ef7595eaefe8e0
Jangjinhyeok <zero9936@gmail.com>
2025-06-02 19:37:34 +0900

    치트키 설정, 침대 9시 이전에 수면 불가로 설정

--- Source/ProjectISG/Systems/Time/Bed.cpp
+++ Source/ProjectISG/Systems/Time/Bed.cpp
@@ -86,6 +86,15 @@ void ABed::GetLifetimeReplicatedProps(
 void ABed::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (TimeManager->GetHour() < 9)
+	{
+		CanInteractive = false;
+	}
+	else
+	{
+		CanInteractive = true;
+	}
 }
 
 void ABed::OnInteractive(AActor* Causer)
